4195e59a10b4e7c72430d3026f9e94b3d1e7e856
Sandeep Kumar <sandeepgiri21487@gmail.com>
2026-04-01 20:45:20 +0530

    add routers back

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from app.routers import users, job, dashboard, auth, application
 
 app = FastAPI()
 
+app.include_router(users.router)
+app.include_router(job.router)
+app.include_router(dashboard.router)
+app.include_router(auth.router)
+app.include_router(application.router)
+
 @app.get("/")
 def home():
-    return {"message":  "WORKING"}
+    return {"message": "Job Portal Live 🚀"}
