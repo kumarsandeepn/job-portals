e35d5e9da7cc303754ea2e9bf9d463cf62733493
Sandeep Kumar <sandeepgiri21487@gmail.com>
2026-04-03 17:13:24 +0530

    fix auth router typo

--- app/main.py
+++ app/main.py
@@ -2,14 +2,16 @@ from fastapi import FastAPI
 from app.database import engine
 from app import models
 from app.routers import job
+from app.routers import auth
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
-# 👇 ये line MUST होनी चाहिए
+# ✅ routers include
 app.include_router(job.router)
+app.include_router(auth.router)
 
 @app.get("/")
 def root():
-    return {"message": "Working 🚀"}
+    return {"message": "working 🚀"}
