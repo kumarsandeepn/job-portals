548aeef142653d7f64b2e585b6286d406b272c62
Sandeep Kumar <sandeepgiri21487@gmail.com>
2026-04-03 17:40:13 +0530

    final auth setup

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,4 @@
-rom fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app import models, schemas
