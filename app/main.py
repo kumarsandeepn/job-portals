01c8043328b20761c39333bffa2f28ab2131bd89
Sandeep Kumar <sandeepgiri21487@gmail.com>
2026-04-03 16:24:01 +0530

    fix db create

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from app.database import engine
+from app import models
 from app.routers import job
 
 app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
 
 # 👇 ये line MUST होनी चाहिए
 app.include_router(job.router)
