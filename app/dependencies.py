ef45f6f547aa6cb529cf00b1bdf943974cf3f66f
Sandeep Kumar <sandeepgiri21487@gmail.com>
2026-04-04 15:41:23 +0530

    fix auth 422

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,24 +1,13 @@
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
-from jose import JWTError, jwt
+from jose import jwt, JWTError
 
 SECRET_KEY = "mysecretkey"
 ALGORITHM = "HS256"
 
-# ✅ token extractor
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 
-# ✅ verify token
-def verify_access_token(token: str):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        return payload
-    except JWTError:
-        raise HTTPException(status_code=401, detail="Invalid Token")
-
-
-# ✅ get current user
 def get_current_user(token: str = Depends(oauth2_scheme)):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
@@ -26,7 +15,7 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
 
-# ✅ role check
+
 def role_required(role: str):
     def checker(user = Depends(get_current_user)):
         if user.get("role") != role:
